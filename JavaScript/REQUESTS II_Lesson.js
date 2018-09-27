4bba8c57a1b490c9e12ef7bba908acf02e766d4d
kenziehong <dinhthianhhong.uel@gmail.com>
2018-09-27 23:14:21 +0700

    Learn JavaScript: Requests

--- JavaScript/REQUESTS II_Lesson.js
+++ JavaScript/REQUESTS II_Lesson.js
@@ -258,3 +258,118 @@ const renderJsonResponse = (res) => {
   // Manipulates responseField to show the returned JSON.
   responseField.innerHTML = `<pre>${rawJson}</pre>`;
 }
+
+
+--------------------------------------------------------------
+//fetch() POST Requests II
+//file main.js
+
+//1.Call the fetch() function, pass it the URL below as a string as its first argument, and pass it an empty object ({}) as its second argument. We’re calling fetch() and providing an endpoint. In the next step we’ll fill in the empty object with the necessary information.
+fetch('https://api-to-call.com/endpoint',({
+  
+  //2.The settings object you passed to the fetch() function will contain two properties: method, with a value of 'POST', and body, with a value of JSON.stringify({id: '200'}). This second argument determines that this request is a POST request and what information will be sent to the API.
+  method: 'POST',
+  body: JSON.stringify({id : '200'})
+})).then(
+  
+  //3.Chain a .then() method to the fetch() function and pass it the success callback function as its first parameter. Pass in response as an argument for the callback function. Leave the code block of the callback function empty for now. The code inside .then() will execute when the promise returned from fetch() is resolved
+  response=>{
+    
+    //4.Inside the callback function's code block, check the ok property of the response object inside of a conditional statement. In the code block of the conditional statement, return response.json(). When returned, this information will be passed on to the next .then() callback function
+    if(response.ok){ return response.json(); }
+    
+    //5.Below the curly braces of the conditional statement, create a new error with this code:.This error will be raised if we get back some status error.
+    throw new Error('Request failed!');
+  },
+  
+  //6.Create the failure callback function. This function takes a single parameter, networkError. Separate the first callback function from the second with a comma. This function is still inside of the .then() method. In the code block of the function you just made, log networkError.message to the console
+  networkError=>{ console.log(networkError.message); }
+).then(
+
+  //7.Chain another .then() method to the end of the first .then() method. In the new .then() method, create an arrow callback function that takes jsonResponse as its parameter. Then in the code block return jsonResponse. The purpose of this step is to view the JSON that was returned from the previous .then().
+  jsonResponse => { return jsonResponse; }
+);
+
+
+----------------------------------------------------------------------------
+//fetch() POST Requests V
+//file main.js
+
+// Information to reach API
+const apiKey = 'b3e68237f76c493ea4386c03e6db44e6';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  fetch(url,
+        ({method: 'POST', headers: { 'Content-type': 'application/json', 'apikey': apiKey }, body: data})
+       ).then(
+    response=>{
+    if(response.ok){return response.json();}
+    throw new Error('Request failed!')
+    },
+    networkError=>{console.log(networkError.message);}
+  ).then(
+    jsonResponse=>{renderResponse(jsonResponse)}
+  );
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild)
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+//fetch() POST Requests V
+//file helperFunction.js
+
+// Manipulates responseField to render a formatted and appropriate message
+const renderResponse = (res) => {
+  // Displays either message depending on results
+  if(res.errors){
+    responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+  } else {  
+    responseField.innerHTML = `<p>Your shortened url is: </p><p> ${res.shortUrl} </p>`;
+  }
+}
+
+// Manipulates responseField to render an unformatted response
+const renderRawResponse = (res) => {
+  // Displays either message depending on results
+  if(res.errors){  
+    responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+  } else {
+    // Adds line breaks for JSON
+    let structuredRes = JSON.stringify(res).replace(/,/g, ", \n");
+    structuredRes = `<pre>${structuredRes}</pre>`;
+    responseField.innerHTML = `${structuredRes}`;
+  }
+}
+
+// Renders the JSON that was returned when the Promise from fetch resolves.
+const renderJsonResponse = (response) => {
+  // Creates an empty object to store the JSON in key-value pairs
+  let rawJson = {}
+  for(let key in response){
+    rawJson[key] = response[key]
+  }
+  // Converts JSON into a string and adding line breaks to make it easier to read
+  rawJson = JSON.stringify(rawJson).replace(/,/g, ", \n")
+  // Manipulates responseField to show the returned JSON.
+  responseField.innerHTML = `<pre>${rawJson}</pre>`
+}
