85e552de92757db04663c2f13e1baef8abffbf0e
kenziehong <dinhthianhhong.uel@gmail.com>
2018-11-02 16:42:34 +0700

    Learn Python-STUDENT BECOMES THE TEACHER-Project

--- Python/LEARN PYTHON_Project.py
+++ Python/LEARN PYTHON_Project.py
@@ -193,6 +193,72 @@ play_RPS()
 
 
 
+----------------------------------------------------------
+
+STUDENT BECOMES THE TEACHER
+
+lloyd = {
+  "name": "Lloyd",
+  "homework": [90.0, 97.0, 75.0, 92.0],
+  "quizzes": [88.0, 40.0, 94.0],
+  "tests": [75.0, 90.0]
+}
+alice = {
+  "name": "Alice",
+  "homework": [100.0, 92.0, 98.0, 100.0],
+  "quizzes": [82.0, 83.0, 91.0],
+  "tests": [89.0, 97.0]
+}
+tyler = {
+  "name": "Tyler",
+  "homework": [0.0, 87.0, 75.0, 22.0],
+  "quizzes": [0.0, 75.0, 78.0],
+  "tests": [100.0, 100.0]
+}
+
+# Add your function below!
+def average(numbers):
+  total = float(sum(numbers))
+  return total/len(numbers)
+  
+def get_average(student):
+  homeword = average(student["homework"])
+  #print"homeword" +str(homeword)
+  quizzes = average(student["quizzes"])
+  #print "quizzes" +str(quizzes)
+  tests = average(student["tests"])
+  #print"tests" + str(tests)
+
+  #The \ character is a continuation character. 
+  #The following line is considered a continuation of the current line.
+
+  return 0.1*homeword + \
+0.3*quizzes + \
+tests*0.6
+
+def get_letter_grade(score):
+  if score>=90:
+    return "A"
+  elif score>=80:
+    return "B"
+  elif score>=70:
+    return "C"
+  elif score>=60:
+    return "D"
+  else:
+    return "F"
+  
+#print(get_letter_grade(get_average(lloyd)))  
+#print(get_average(tyler))
+
+def get_class_average(class_list):
+  results = []
+  for student in class_list:
+    results.append(get_average(student))
+  return average(results)  
+
+students = [alice, lloyd, tyler]
+print(get_letter_grade(get_class_average(students)))
 
 
 
