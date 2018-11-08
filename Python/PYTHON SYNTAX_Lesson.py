747af5b00e14ad9cd1bae451cf936b7499f00241
kenziehong <dinhthianhhong.uel@gmail.com>
2018-11-08 16:38:14 +0700

    Introduction to Classes

--- Python/PYTHON SYNTAX_Lesson.py
+++ Python/PYTHON SYNTAX_Lesson.py
@@ -1430,9 +1430,166 @@ def flip_bit(number, n):
 
 print(flip_bit(12,4)) #0b100
 
+-------------------------------------------------------------------------------------
+INTRODUCTION TO CLASSES
+# Class definition
+
+class Animal(object):
+  """Makes cute animals."""
+  # For initializing our instance objects
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+# Note that self is only used in the __init__()
+# function definition; we don't need to pass it
+# to our instance objects.
+
+zebra = Animal("Jeffrey", 2, True)
+giraffe = Animal("Bruce", 1, False)
+panda = Animal("Chad", 7, True)
+
+print zebra.name, zebra.age, zebra.is_hungry #Jeffrey 2 True
+print giraffe.name, giraffe.age, giraffe.is_hungry
+print panda.name, panda.age, panda.is_hungry
 
 
+++++++++++++++++++++++++++++++++++++
+
+class ShoppingCart(object):
+  """Creates shopping cart objects
+  for users of our fine website."""
+  
+  def __init__(self, customer_name):
+    self.customer_name = customer_name
+    self.items_in_cart = {}
+  def add_item(self, product, price):
+    """Add product to the cart."""
+    if not product in self.items_in_cart:
+      self.items_in_cart[product] = price
+      print product + " added."
+    else:
+      print product + " is already in the cart."
+
+  def remove_item(self, product):
+    """Remove product from the cart."""
+    if product in self.items_in_cart:
+      del self.items_in_cart[product]
+      print product + " removed."
+    else:
+      print product + " is not in the cart."
+
+my_cart = ShoppingCart("Hong_cart")
+my_cart.add_item("Rose", 13)
+
+++++++++++++++++++++++++++++++++
+
+INTRODUCTION TO CLASSES
+Warning: Here Be Dragons
+Inheritance is a tricky concept
 
 
+class Customer(object):
+  """Produces objects that represent customers."""
+  def __init__(self, customer_id):
+    self.customer_id = customer_id
+
+  def display_cart(self):
+    print "I'm a string that stands in for the contents of your shopping cart!"
+
+class ReturningCustomer(Customer):
+  """For customers of the repeat variety."""
+  def display_order_history(self):
+    print "I'm a string that stands in for your order history!"
+
+monty_python = ReturningCustomer("ID: 12345")
+monty_python.display_cart()
+monty_python.display_order_history()
+"""
+I'm a string that stands in for the contents of your shopping cart!
+I'm a string that stands in for your order history!
+"""
+
++++++++++++++++++++++++++++++++++++
+Inheritance
+
+
+class Shape(object):
+  """Makes shapes!"""
+  def __init__(self, number_of_sides):
+    self.number_of_sides = number_of_sides
+
+# Add your Triangle class below!
+class Triangle(Shape):
+  def __init__(self, side1, side2, side3):
+    self.side1 = side1
+    self.side2 = side2
+    self.side3 = side3
+
+
++++++++++++++++++++++++++++++++++
+Override
+
+
+class Employee(object):
+  """Models real-life employees!"""
+  def __init__(self, employee_name):
+    self.employee_name = employee_name
+
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 20.00
+
+# Add your code below!
+#PartTimeEmployee.calculate_wage overrides Employee.calculate_wage
+class PartTimeEmployee(Employee):
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours*12.00
+
+ #you've overwritten a method or attribute defined in that class' base class (also called a parent or superclass)
+  #You can directly access the attributes or methods of a superclass with Python's built-in super call.
+  def full_time_wage(self, hours):
+    return super(PartTimeEmployee, self).calculate_wage(hours)
+
+milton =  PartTimeEmployee("Hong")
+print milton.full_time_wage(10)    
+
+
+++++++++++++++++++++++++++++++++++
+Basic Class
+
+class Triangle(object):
+  #2.add a member varibale (attribue)
+  number_of_sides = 3
+  
+  #1.
+  def __init__(self, angle1, angle2, angle3):
+    self.angle1 = angle1 # a instance variable (attribute)
+    self.angle2 = angle2
+    self.angle3 = angle3
+  
+  #3. add a function (method)
+  def check_angles(self):
+    return self.angle1 + self.angle2 + self.angle3 == 180
+
+
+#4. create an instance 
+my_triangle = Triangle(90,30,60)
+print my_triangle.number_of_sides 
+print my_triangle.check_angles()
+    
+#5. create a class that inherrits 
+class Equilateral(Triangle):
+  
+  #add a member variable
+  angle = 60
+  
+  #overwritten attribute
+  def __init__(self):
+    self.angle1 = self.angle
+    self.angle2 = self.angle
+    self.angle3 = self.angle
+    
 
 
