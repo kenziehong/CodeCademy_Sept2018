fcb8b9834de1147bd185c459c934e400ad08e7be
kenziehong <dinhthianhhong.uel@gmail.com>
2018-11-14 15:40:38 +0700

    SQL-Basic

---
+++ DataScience/Lesson_DataScience.sql
@@ -0,0 +1,169 @@
+QUERIES
+Select
+
+SELECT is used every time you want to query data from a database and * means all columns.
+#Line breaks don't mean anything specific in SQL. We could write this entire query in one line, and it would run just fine.
+
+#select the name and genre columns of the table.
+SELECT name, genre, year
+FROM movies;
+
+++++++++++++++++++++++++++++++++++
+
+QUERIES
+As
+
+AS is a keyword in SQL that allows you to rename a column or table using an alias.
+
+#select the imdb_rating column and rename it as IMDb.
+SELECT imdb_rating AS 'IMDb' FROM movies;
+
++++++++++++++++++++++++++++++++++
+
+QUERIES
+Distinct
+
+DISTINCT is used to return unique values in the output. It filters out all duplicate values in the specified column(s).
+
+#return the unique values of the year column instead.
+SELECT DISTINCT year FROM movies;
+
++++++++++++++++++++++++++++++++++
+
+QUERIES
+Where
+
+We can restrict our query results using the WHERE clause in order to obtain only the information we want.
+WHERE clause filters the result set to only include rows where the following condition is true.
+
+#retrieve all the recent movies, specifically those that were released after 2014.
+SELECT * FROM movies WHERE year > 2014;
+
+++++++++++++++++++++++++++++++++++
+
+QUERIES
+Like I
+
+when you want to compare similar values.
+LIKE is a special operator used with the WHERE clause to search for a specific pattern in a column.
+
+The _ means you can substitute any individual character here without breaking the pattern. 
+
+#select all movies that start with 'Se' and end with 'en' and have exactly one character in the middle
+SELECT * FROM movies WHERE name LIKE 'Se_en';
+
++++++++++++++++++++++++++++++++++
+
+QUERIES
+Like II
+
+% is a wildcard character that matches zero or more missing letters in the pattern. 
+We can also use % both before and after a pattern:
+
+#selects all the information about the movie titles that begin with the word 'The'.
+SELECT * FROM movies WHERE name LIKE 'The %';
+
++++++++++++++++++++++++++++++++++++
+
+QUERIES
+Is Null
+
+Unknown values are indicated by NULL
+It is not possible to test for NULL values with comparison operators, such as = and !=.
+
+#Write a query to find all the movies without an IMDb rating.
+#Select only the name column!
+SELECT name FROM movies WHERE imdb_rating IS NULL;
+
+
+++++++++++++++++++++++++++++++++++++++++
+
+QUERIES
+Between
+
+filter the result set within a certain range
+
+#select all information about movies whose name begins with the letter 'D','E','F' 
+#(BETWEEN two letters is not inclusive of the 2nd letter.)
+SELECT * FROM movies WHERE name BETWEEN 'D' AND 'G';
+
+#selects all information about movies that were released in the 1970's 
+#(BETWEEN two numbers is inclusive of the 2nd number.)
+SELECT * FROM movies WHERE year BETWEEN 1970 AND 1979;
+
+
+++++++++++++++++++++++++++++++++++
+
+QUERIES
+And
+Or
+
+Sometimes we want to combine multiple conditions in a WHERE clause to make the result set more specific and useful.
+AND operator displays a row if all the conditions are true.
+OR operator displays a row if any condition is true.
+
+#Now, let's retrieve every movie released in the 70's, that's also well received.
+SELECT * FROM movies 
+WHERE year BETWEEN 1970 AND 1979 
+  AND imdb_rating > 8;
+
+
+#write a new query that selects all movies made prior to 1985 that are also in the horror genre.
+SELECT * FROM movies 
+WHERE year < 1985 
+  AND genre = 'horror';
+
+SELECT * FROM movies
+WHERE year > 2014
+   OR genre ='action';
+  
+SELECT * FROM movies
+WHERE genre = 'romance' 
+   OR genre ='comedy';
+    
+++++++++++++++++++++++++++++++++++
+
+QUERIES
+Order By
+
+ORDER BY is a clause that indicates you want to sort the result set by a particular column.
+Note: ORDER BY always goes after WHERE (if WHERE is present).
+
+#retrieve the name, year columns of all the movies, ordered by their name alphabetically
+SELECT name, year FROM movies ORDER BY name;
+
+#retrieves the name, year, and imdb_rating columns of all the movies, ordered highest to lowest by their ratings.
+SELECT name, year, imdb_rating FROM movies ORDER BY imdb_rating DESC;    
+
+
++++++++++++++++++++++++++++++++++
+QUERIES
+Limit
+
+LIMIT is a clause that lets you specify the maximum number of rows the result set will have. This saves space on our screen and makes our queries run faster.
+
+#write a query that returns the top 3 highest rated movies.Select all the columns.
+SELECT * FROM movies ORDER BY imdb_rating DESC LIMIT 3;
+
++++++++++++++++++++++++++++++++++
+
+QUERIES
+Case
+
+A CASE statement allows us to create different outputs (usually in the SELECT statement).
+Each WHEN tests a condition and the following THEN gives us the string if the condition is true.
+The ELSE gives us the string if all the above conditions are false.
+The CASE statement must end with END
+
+
+#Select the name column and use a CASE statement to create the second column that is. Rename the whole CASE statement to 'Mood' using AS
+SELECT name,
+ CASE
+  WHEN genre = 'romance' THEN 'Chill'
+  WHEN genre = 'comedy' THEN 'Chill'
+  ELSE 'Intense'
+ END 
+ AS 'Mood'
+FROM movies; 
+ 
+ 
