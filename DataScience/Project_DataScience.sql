eb8c526cb00b64dffe39130f49a537593af989c2
kenziehong <dinhthianhhong.uel@gmail.com>
2018-11-14 16:50:55 +0700

    WRITING QUERIES New York Restaurants Project

--- DataScience/Project_DataScience.sql
+++ DataScience/Project_DataScience.sql
@@ -73,3 +73,40 @@ SELECT title, publisher FROM news ORDER BY title ASC;
 SELECT * FROM news WHERE category ='b' 
 ORDER BY timestamp DESC 
 LIMIT 20;
+
+---------------------------------------------------------------
+
+/*1. Start by getting a feel for the nomnom table:*/
+SELECT * FROM nomnom;
+
+/*2.What are the distinct neighborhoods?*/
+SELECT DISTINCT neighborhood FROM nomnom;
+
+/*3.What are the distinct cuisine types?*/
+SELECT DISTINCT cuisine FROM nomnom;
+
+/*4.Suppose we would like some Chinese takeout.
+What are our options?*/
+SELECT * FROM nomnom WHERE cuisine ='Chinese';
+
+/*5.Return all the restaurants with reviews of 4 and above.*/
+SELECT * FROM nomnom WHERE review >= 4;
+
+
+/**8.Find all the close by spots in Midtown, Downtown or Chinatown..*/
+SELECT * FROM nomnom WHERE neighborhood = 'Midtown' OR neighborhood = 'Downtown' OR neighborhood = 'Chinatown';
+
+/**11.Use a CASE statement to change the rating system to:.*/
+SELECT review,
+  CASE
+   WHEN review>4.5 THEN 'Extraordinary'
+   WHEN review>4 THEN 'Excellent'
+   WHEN review>3 THEN 'Good'
+   WHEN review>2 THEN 'Fair'
+   ELSE 'Poor'
+  END AS 'status'
+FROM nomnom;  
+  
+
+
+
