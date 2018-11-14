996159aeb93f5b0717ebe931ea4638717123ec26
kenziehong <dinhthianhhong.uel@gmail.com>
2018-11-14 16:31:49 +0700

    Code Challenge-Queries

--- DataScience/Project_DataScience.sql
+++ DataScience/Project_DataScience.sql
@@ -31,3 +31,45 @@ DELETE FROM friends WHERE id = 1;
 
 SELECT * FROM friends;
 
+---------------------------------------------------------
+Code Challenge
+Queries
+
+#Find the number of girls who were named Lillian for the full span of time of the database.
+#Select only the year and number columns.
+
+SELECT year, number FROM babies WHERE name = 'Lillian';
+
+#Find 20 distinct names that start with 'S'.
+#Select only the name column.
+SELECT DISTINCT name FROM babies WHERE name LIKE 'S%' LIMIT 20;
+
+#What are the top 10 names in 1880?
+#Select the name, gender, and number columns.
+
+SELECT name, gender, number FROM babies 
+WHERE year = 1880
+ORDER BY number DESC
+LIMIT 10;
+
+#Return all the restaurants that are Italian and $$$.
+#Select all the columns.
+SELECT * FROM nomnom WHERE cuisine ='Italian' AND price = '$$$';
+
+#Your coworker Trey can't remember the exact name of a restaurant he went to but he knows it contains the word 'meatball' in it.
+#Select all the columns.
+SELECT * FROM nomnom WHERE name LIKE '%meatball%'; 
+
+#Some of the restaurants have not been inspected yet or are currently appealing their health grade score.
+#Find the restaurants that have empty health values.
+SELECT * FROM nomnom WHERE health IS NULL; 
+
+ 
+#Order the table by title (from A-Z).
+#Select only the title and publisher columns.
+SELECT title, publisher FROM news ORDER BY title ASC;
+
+#What are the 20 business articles published most recently?
+SELECT * FROM news WHERE category ='b' 
+ORDER BY timestamp DESC 
+LIMIT 20;
