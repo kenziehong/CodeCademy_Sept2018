79212d597525a362821676ae4630f83a69577f09
kenziehong <dinhthianhhong.uel@gmail.com>
2018-11-24 09:15:30 +0700

    Introudction to Numpy_Betty's Bakery Project

--- DataScience/Project_DataScience.sql
+++ DataScience/Project_DataScience.sql
@@ -318,7 +318,25 @@ ORDER BY trips_completed DESC
 LIMIT 2;
  
  
- 
+--------------------------------------------------
+
+INTRODUCTION TO NUMPY
+#Betty's Bakery Project
+
+import numpy as np
+cupcakes = np.array([2, 0.75, 2, 1, 0.5])
+recipes = np.genfromtxt('recipes.csv', delimiter =',')
+print(recipes)
+
+eggs = recipes[:,2]
+one_egg = recipes[eggs==1]
+print(one_egg)
+
+cookies = recipes[2,:]
+double_batch = cupcakes*2
+
+grocery_list = cookies + double_batch
+print(grocery_list) 
  
  
  
