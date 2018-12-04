07ac3305ffbf1760fa7d6def7271eca03007dc9b
kenziehong <dinhthianhhong.uel@gmail.com>
2018-12-04 10:04:03 +0700

    Petal Power Inventory - Pandas Project

--- DataScience/Project_DataScience.sql
+++ DataScience/Project_DataScience.sql
@@ -412,7 +412,46 @@ print(percentage_ceballos) #0.471428571429
 print(ceballos_loss_surveys) #0.213
 print(ceballos_loss_new) #0
  
- 
+--------------------------------------------------------------------
+4/12/2018
+
+INTRODUCTION TO PANDAS
+Petal Power Inventory
+
+#You're the lead data analyst for a chain of gardening stores called Petal Power. Help them analyze their inventory!
+
+import codecademylib
+import pandas as pd
+
+#1. Load the data into a DataFrame called inventory
+inventory = pd.read_csv('inventory.csv')
+
+#3.select these rows and save them to staten_island
+staten_island = inventory.head(10)
+
+#4. select the column product_description from staten_island and save it to the variable product_request
+product_request = staten_island.product_description
+
+#5. select all rows where location is equal to Brooklyn and product_type is equal to seeds and save them to the variable seed_request
+seed_request = inventory[(inventory.location == 'Brooklyn') & (inventory.product_type == 'seeds')]
+
+#6. add a column to inventory called in_stock which is True if quantity is greater than 0 and False if quantity equals 0
+inventory['in_stock'] = inventory.apply(lambda row: True if row.quantity > 0 else False, axis = 1)
+
+#7. create a column called total_value that is equal to price multiplied by quantity
+inventory['total_value'] = inventory.apply(lambda row: row.quantity * row.price, axis =1 )
+
+#8. 
+combine_lambda = lambda row: \
+                 '{} - {}'.format(row.product_type, row.product_description)
+
+#9. using combine_lambda, create a new column in ventory called full_description of each product
+inventory['full_description'] = inventory.apply(combine_lambda, axis =1)
+  
+print(inventory.head(10))
+print(product_request)
+print(seed_request)
+
  
  
  
