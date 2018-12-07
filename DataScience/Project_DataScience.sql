8ba92a18915d043cd7390ea300cdfd0bf0223b0e
kenziehong <dinhthianhhong.uel@gmail.com>
2018-12-07 17:51:10 +0700

    Recreate Graphs Using Matplotlib - Project

--- DataScience/Project_DataScience.sql
+++ DataScience/Project_DataScience.sql
@@ -539,3 +539,275 @@ a_clicks_pivot['percentage_clicked'] = a_clicks_pivot[True] / (a_clicks_pivot[Tr
 print(a_clicks_pivot)
 
 
+---------------------------------------------------------------------
+
+RECREATE GRAPHS USING MATPLOTLIB!
+Bar Chart with Error
+
+#First, we are going to look at the chart called bar_chart.png.
+
+import codecademylib
+from matplotlib import pyplot as plt
+
+past_years_averages = [82, 84, 83, 86, 74, 84, 90]
+years = [2000, 2001, 2002, 2003, 2004, 2005, 2006]
+error = [1.5, 2.1, 1.2, 3.2, 2.3, 1.7, 2.4]
+
+# Make your chart here
+
+#1. First, create a figure of width 10 and height 8
+plt.figure(figsize=(10,8))
+
+#2. Plot the blue bars, which have the heights listed in past_years_averages
+#3.Add error bars of cap size 5 and heights corresponding to the list error
+plt.bar(years, past_years_averages, color = 'blue', yerr = error, capsize = 5)
+
+#4. Set the axis to go from -0.5 to 6.5 on the x-axis and 70 to 95 on the y-axis
+plt.axis([-0.5, 6.5, 70, 95])
+
+#5. Create an ax object using plt.subplot(). Use ax to set the x-axis ticks to be range(len(year)) and the x-axis labels to be years list
+ax = plt.subplot()
+ax.set_xticks(range(len(years)))
+ax.set_xticklabels(years)
+
+#6. Add the title, x-axis label and y-axis label
+plt.title("Final Exam Averages")
+plt.xlabel("Year")
+plt.ylabel("Test average")
+
+#7. Save your figure to a file called my_bar_chart.png
+plt.savefig("my_bar_chart.png")
+plt.show()
+
+----------------------------------------------------------------
+
+RECREATE GRAPHS USING MATPLOTLIB!
+Side By Side Bars
+
+/*Now, we are going to look at the chart called side-by-side.png. This displays the differences in average test scores 
+between students who went to two different middle schools before enrolling in your high school.
+*/
+import codecademylib
+from matplotlib import pyplot as plt
+
+unit_topics = ['Limits', 'Derivatives', 'Integrals', 'Diff Eq', 'Applications']
+middle_school_a = [80, 85, 84, 83, 86]
+middle_school_b = [73, 78, 77, 82, 86]
+
+def create_x(t, w, n, d):
+    return [t*x + w*n for x in range(d)]
+  
+#1. Using create_x, make the lists school_a_x and school_b_x which will determine where to put the bars for Middle School A and Middle School B along the x-axis
+
+#For school_a_x, use the following inputs to create_x:
+#t = 2 # There are two sets of data: A and B
+#w = 0.8 # We generally want bars to be 0.8
+#n = 1 # A is first set of data
+#d = 5 # There are 5 topics we're plotting
+school_a_x = create_x(2, 0.8, 1, 5)
+school_b_x = create_x(2, 0.8, 2, 5)
+
+#2.Create a figure of width 10 and height 8.
+plt.figure(figsize=(10,8))
+
+#3. Create a set of axes and save them to ax.
+ax = plt.subplot()  
+
+#4.Plot a set of bars representing middle_school_a and a set representing middle_school_b next to each other on the same graph.
+plt.bar(school_a_x, middle_school_a)
+plt.bar(school_b_x,middle_school_b)
+
+#5.Create a new list of x-values called middle_x, which are the values in the middle of school_a_x and school_b_x. This is where we will place the x-ticks. Look at the final graph to see this placement.
+middle_x = [ (a + b) / 2.0 for a, b in zip(school_a_x, school_b_x)]
+
+#6.Set the x-ticks to be the middle_x list.
+ax.set_xticks(middle_x)
+
+#7.Set the x-tick labels to be the list unit_topics.
+ax.set_xticklabels(unit_topics)
+
+#8.Create a legend, as shown in the final graph, that labels the first set of bars Middle School A and the second set of bars Middle School B
+plt.legend(["Middle School A", "Middle School B"])
+
+#9.Create a title ("Test Averages on Different Units"), x-axis label ("Unit"), and y-axis label ("Test Average").
+plt.title('Test Averages on Different Units')
+plt.ylabel('test average')
+plt.xlabel('unit')
+plt.show()
+
+#10.Save your figure to a file called my_side_by_side.png.
+plt.savefig('my_side_by_side.png')
+
+++++++++++++++++++++++++++++++++++++++++++++++++++
+
+RECREATE GRAPHS USING MATPLOTLIB!
+Stacked Bars
+
+/*Now, we are going to look at the chart called stacked-bars.png. 
+This graph displays the breakdown of students who got As, Bs, Cs, Ds, and Fs in each unit.
+*/
+import codecademylib
+from matplotlib import pyplot as plt
+import numpy as np
+
+unit_topics = ['Limits', 'Derivatives', 'Integrals', 'Diff Eq', 'Applications']
+As = [6, 3, 4, 3, 5]
+Bs = [8, 12, 8, 9, 10]
+Cs = [13, 12, 15, 13, 14]
+Ds = [2, 3, 3, 2, 1]
+Fs = [1, 0, 0, 3, 0]
+
+x = range(5)
+
+#create d_bottom and f_bottom here
+#1. The Bs bars will go on top of the As bars, but at what heights will the Cs, Ds, and Fs bars start? 
+#The bottom of the bars representing the Cs will be at the height of the As plus the Bs. We can do this in NumPy (a scientific computing package for Python) with the np.add function. c_bottom, the starting heights for the Cs
+#Underneath the definition of c_bottom, define d_bottom (where the Cs end), and f_bottom (where the Ds end).
+c_bottom = np.add(As, Bs)
+d_bottom = np.add(c_bottom, Cs)
+f_bottom = np.add(d_bottom, Ds)
+
+#create your plot here
+#2.Create a figure of width 10 and height 8
+plt.figure(figsize=(10,8))
+
+#3.Plot the As, Bs, Cs, Ds, and Fs. Give each one the appropriate bottom list that will stack them on top of each other
+plt.bar(x, As)
+plt.bar(x, Bs, bottom=As)
+plt.bar(x, Cs, bottom=c_bottom)
+plt.bar(x, Ds, bottom=d_bottom)
+plt.bar(x, Fs, bottom=f_bottom)
+
+#4.Create a set of axes and save them to ax
+ax = plt.subplot()
+
+#5.Set the x-ticks to be range(len(unit_topics))
+ax.set_xticks(range(len(unit_topics)))
+
+#6.Set the x-tick labels to be the unit_topics
+ax.set_xticklabels(unit_topics)
+
+#7.Give the plot the title you see in the final graph, and the same x-axis label and y-axis label.
+plt.title('Grade distribution')
+plt.xlabel('Unit')
+plt.ylabel('Number of Students')
+plt.show()
+
+#8.Save your figure to a file called my_stacked_bar.png.
+plt.savefig('my_stacked_bar.png')
+
+++++++++++++++++++++++++++++++++++++++++
+
+RECREATE GRAPHS USING MATPLOTLIB!
+Two Histograms on a Plot
+
+/*Now, we are going to look at the chart called histograms.png. 
+This displays the breakdown of final exam scores between your first year of teaching vs your second year of teaching.
+*/
+import codecademylib
+from matplotlib import pyplot as plt
+
+exam_scores1 = [62.58, 67.63, 81.37, 52.53, 62.98, 72.15, 59.05, 73.85, 97.24, 76.81, 89.34, 74.44, 68.52, 85.13, 90.75, 70.29, 75.62, 85.38, 77.82, 98.31, 79.08, 61.72, 71.33, 80.77, 80.31, 78.16, 61.15, 64.99, 72.67, 78.94]
+exam_scores2 = [72.38, 71.28, 79.24, 83.86, 84.42, 79.38, 75.51, 76.63, 81.48,78.81,79.23,74.38,79.27,81.07,75.42,90.35,82.93,86.74,81.33,95.1,86.57,83.66,85.58,81.87,92.14,72.15,91.64,74.21,89.04,76.54,81.9,96.5,80.05,74.77,72.26,73.23,92.6,66.22,70.09,77.2]
+
+# Make your plot here
+#1.Create a figure of width 10 and height 8
+plt.figure(figsize=(10, 8))
+
+#2.Make a histogram of the exam_scores1, normalized, with 12 bins.
+plt.hist(exam_scores1, bins=12, normed=True, histtype='step', linewidth=2)
+
+#3.Make a histogram of the exam_scores2, normalized, with 12 bins.
+plt.hist(exam_scores2, bins=12, normed=True, histtype='step', linewidth=2)
+
+#4.Add histtype = 'step' to each plt.hist call, in order to make these histograms more visible.
+#We can also make the lines thicker by setting the linewidth inside the calls to plt.hist.
+#Let's set the linewidth to be 2 for both histograms.
+
+#5.Create a legend showing that the first set of data is "1st Yr Teaching" and the second set of data is "2nd Yr Teaching"
+plt.legend(["1st Yr Teaching", "2nd Yr Teaching"])
+
+#6.Add the title, x-axis label, and y-axis label that you see on the final graph.Add the title, x-axis label, and y-axis label that you see on the final graph.
+plt.title("Final Exam Score Distribution")
+plt.xlabel("Percentage")
+plt.ylabel("Frequency")
+
+plt.show()
+
+#7.Save your figure to a file called my_histogram.png.
+plt.savefig('my_histogram.png')
+
++++++++++++++++++++++++++++++++++++
+
+RECREATE GRAPHS USING MATPLOTLIB!
+Labeled Pie Chart
+
+/*Now, we are going to look at the chart called pie.png. 
+This displays what students think the hardest topic covered throughout your math course is.
+*/
+import codecademylib
+from matplotlib import pyplot as plt
+
+unit_topics = ['Limits', 'Derivatives', 'Integrals', 'Diff Eq', 'Applications']
+num_hardest_reported = [1, 3, 10, 15, 1]
+
+#Make your plot here
+#1.Create a figure of width 10 and height 8.
+plt.figure(figsize=(10,8))
+
+#2.Plot the num_hardest_reported list as a pie chart.
+#4.Label the slices with the unit_topics list and put a percentage label on each slice, rounded to the nearest int.
+plt.pie(num_hardest_reported, labels=unit_topics, autopct="%1d%%")
+
+#3.Set the axes to be 'equal'
+plt.axis('equal')
+
+#5.Add the title "Hardest Topics"
+plt.title('Hardest Topics')
+
+plt.show()
+
+#6.Save your figure to a file called my_pie_chart.png.
+plt.savefig("my_pie_chart.png")
+
+++++++++++++++++++++++++++++++++++++++++
+
+RECREATE GRAPHS USING MATPLOTLIB!
+Line with Shaded Error
+
+/*Now, we are going to look at the chart called line_graph.png. 
+This displays the relationship between the time students say they studied and 
+the scores they received on their final exams.
+*/
+
+import codecademylib
+from matplotlib import pyplot as plt
+
+hours_reported =[3, 2.5, 2.75, 2.5, 2.75, 3.0, 3.5, 3.25, 3.25,  3.5, 3.5, 3.75, 3.75,4, 4.0, 3.75,  4.0, 4.25, 4.25, 4.5, 4.5, 5.0, 5.25, 5, 5.25, 5.5, 5.5, 5.75, 5.25, 4.75]
+exam_scores = [52.53, 59.05, 61.15, 61.72, 62.58, 62.98, 64.99, 67.63, 68.52, 70.29, 71.33, 72.15, 72.67, 73.85, 74.44, 75.62, 76.81, 77.82, 78.16, 78.94, 79.08, 80.31, 80.77, 81.37, 85.13, 85.38, 89.34, 90.75, 97.24, 98.31]
+
+#1.Create a figure of width 10 and height 8.
+plt.figure(figsize=(10,8))
+
+# Create your hours_lower_bound and hours_upper_bound lists here 
+#3.Let's assume the error on students' reporting of their hours studying is 20%.
+#Create a list hours_lower_bound, which has the elements of hours_reported minus 20%, and a list hours_upper_bound, which has the elements of hours_reported plus 20%.
+hours_lower_bound = [element - (element * 0.2) for element in hours_reported]
+hours_upper_bound = [element + (element * 0.2) for element in hours_reported]
+
+# Make your graph here
+#2.Plot the exam_scores on the x-axis and the hours_reported on the y-axis, with a linewidth of 2.
+plt.plot(exam_scores, hours_reported, linewidth=2)
+
+#4.Shade the area between hours_lower_bound and hours_upper_bound with an alpha of 0.2
+plt.fill_between(exam_scores, hours_lower_bound, hours_upper_bound, alpha = 0.2)
+
+#5.Give the plot the title 'Time spent studying vs final exam scores', x-axis label 'Score', and y-axis label 'Hours studying (self-reported)'.
+plt.xlabel("Score")
+plt.title("Time spent studying vs final exam scores")
+plt.ylabel('Hours studying (self-reported)')
+plt.show()
+
+#6.Save your figure to a file called my_line_graph.png.
+plt.savefig("my_line_graph.png")
+
