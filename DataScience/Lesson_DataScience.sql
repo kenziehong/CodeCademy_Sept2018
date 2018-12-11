ba369d554f1dff2ca737e02b9bac6136ff728eb2
kenziehong <dinhthianhhong.uel@gmail.com>
2018-12-12 01:24:07 +0700

    Classification: K-Nearest Neighbors - Distance Formula Lesson

--- DataScience/Lesson_DataScience.sql
+++ DataScience/Lesson_DataScience.sql
@@ -3639,5 +3639,157 @@ sales_predict = line_fitter.predict(temperature)
 plt.plot(temperature, sales_predict)
 plt.show()
 
-+++++++++++++++++++++++++++++++++++++++++++++
+----------------------------------------------------------
+
+12/12/2018
+
+DISTANCE FORMULA
+Representing Points
+
+/*In this lesson, you will learn three different ways to define the distance between two points:
+
+Euclidean Distance
+Manhattan Distance
+Hamming Distance
+
+Before diving into the distance formulas, it is first important to consider how to represent points in your code.
+Note that we can only find the difference between two points if they have the same number of dimensions!
+*/
+two_d = [10, 2]
+five_d = [30, -1, 50, 0, 2]
+
+#1. Create a new point named four_d that has four dimensions
+four_d =[1,3,5,3]
+
+++++++++++++++++++++++++++++++++++
+
+DISTANCE FORMULA
+Euclidean Distance
+
+/*Euclidean Distance is the most commonly used distance formula. 
+To find the Euclidean distance between two points, we first calculate the squared distance between each dimension.
+ If we add up all of these squared differences and take the square root, we've computed the Euclidean distance.
+
+hinh tam vuong - tinh canh huyen
+
+*/
+#1. Create a function named auclidean_distance() that takes two lists as parameters named pt1 and pt2
+def euclidean_distance(pt1, pt2):
+  distance = 0
+  
+  #2Create a for loop to loop through the dimensions of each point. Add the squared difference between each dimension to distance.
+  for i in range(len(pt1)):
+    distance += (pt1[i] -pt2[i])**2
+   
+  #3. Outside of the for loop, take a square root of distance 
+  distance = distance**0.5  
+  return distance
+
+#4.
+print(euclidean_distance([1,2],[4,0]))
+print(euclidean_distance([5,4,3],[1,7,9]))
+#print(euclidean_distance([2,3,4],[1,2]))
+
+++++++++++++++++++++++++++++++++
+
+DISTANCE FORMULA
+Manhattan Distance
+
+M/*anhattan Distance is extremely similar to Euclidean distance. Rather than summing the squared difference between each dimension, 
+we instead sum the absolute value of the difference between each dimension. 
+It's called Manhattan distance because it's similar to how you might navigate when walking city blocks. 
+
+If you've ever wondered 
+"how many blocks will it take me to get from point A to point B", you've computed the Manhattan distance.
+
+2 canh ben - tam giac vuong*/
+
+#1. create a function called manhattan_distance() that takes 2 lists named pt1 and pt2 as parameters
+def manhattan_distance(pt1, pt2):
+  distance = 0
+  
+  #2. create a for loop to loop through the dimensions of each point. Add the absolute value of the difference between each dimention to distance
+  for i in range(len(pt1)):
+    distance += abs(pt1[i] -pt2[i])
+    
+  return distance
+
+print(manhattan_distance([1,2], [4,0]))
+print(manhattan_distance([5, 4, 3], [1, 7, 9]))
+
+++++++++++++++++++++++++++++++++++++
+DISTANCE FORMULA
+Hamming Distance
+
+/*Hamming Distance is another slightly different variation on the distance formula. 
+Instead of finding the difference of each dimension, Hamming distance only cares about 
+whether the dimensions are exactly equal. 
+When finding the Hamming distance between two points, add one for every dimension that has different values.
+
+Hamming distance is used in spell checking algorithms. 
+For example, the Hamming distance between the word "there" and the typo "thete" is one. 
+Each letter is a dimension, and each dimension has the same value except for one.
+*/
+
+
+def hamming_distance(pt1, pt2):
+  distance = 0
+  for i in range(len(pt1)):
+    if pt1[i] != pt2[i]:
+      distance +=1
+  return distance
+
+print(hamming_distance([1,2], [1,100]))
+print(hamming_distance([5,4,9] , [1,7,9]))
+
++++++++++++++++++++++++++++++++++++++
+
+DISTANCE FORMULA
+SciPy Distances
+
+N/*ow that you've written these three distance formulas yourself, 
+let's look at how to use them using Python's SciPy library:
+
+Euclidean Distance .euclidean()
+Manhattan Distance .cityblock()
+Hamming Distance .hamming()
+There are a few noteworthy details to talk about:
+
+First, the scipy implementation of Manhattan distance is called cityblock(). 
+Remember, computing Manhattan distance is like asking how many blocks away you are from a point.
+
+Second, the scipy implementation of Hamming distance will always return a number between 0 an 1. 
+Rather than summing the number of differences in dimensions, 
+this implementation sums those differences and then divides by the total number of dimensions. 
+For example, in your implementation, the Hamming distance between [1, 2, 3] and [7, 2, -10] would be 2. 
+In scipy's version, it would be 2/3.
+*/
+from scipy.spatial import distance
+
+def euclidean_distance(pt1, pt2):
+  distance = 0
+  for i in range(len(pt1)):
+    distance += (pt1[i] - pt2[i]) ** 2
+  return distance ** 0.5
+
+def manhattan_distance(pt1, pt2):
+  distance = 0
+  for i in range(len(pt1)):
+    distance += abs(pt1[i] - pt2[i])
+  return distance
+
+def hamming_distance(pt1, pt2):
+  distance = 0
+  for i in range(len(pt1)):
+    if pt1[i] != pt2[i]:
+      distance += 1
+  return distance
+
+print(euclidean_distance([1, 2], [4, 0]))
+print(manhattan_distance([1, 2], [4, 0]))
+print(hamming_distance([5, 4, 9], [1, 7, 9]))
+
+print(distance.euclidean([1,2], [4,0]))
+print(distance.cityblock([1,2], [4,0]))
+print(distance.hamming([5,4,9], [1,7,9]))
 
