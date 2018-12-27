efa519be041169e37ac19b9070a7743045259086
kenziehong <dinhthianhhong.uel@gmail.com>
2018-12-28 01:00:09 +0700

    Twitch Project - Part 2 - Visualize Data with Matplotlib

--- DataScience/Project_DataScience.sql
+++ DataScience/Project_DataScience.sql
@@ -2157,4 +2157,80 @@ The full training .csv files are on GitHub.
 */
 
 ------------------------------------------------------
+TWITCH PROJECT
+Twitch Part 2: Visualize Data with Matplotlib
+Twitch
 
+/*Welcome to Part 2 of the Twitch Project. In this part of the project, you will be taking your findings 
+from the SQL queries and visualize them using Python and Matplotlib, in the forms of:
+
+Bar Graph: Featured Games
+Pie Chart: Stream Viewers' Locations
+Line Graph: Time Series Analysis
+The Twitch Science Team provided this practice dataset. You can download the .csv files (800,000 rows) from GitHub.
+
+Note: This is data is scrubbed and is meant for practice use only.
+*/
+
+import codecademylib3_seaborn
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+
+# Bar Graph: Featured Games
+
+games = ["LoL", "Dota 2", "CS:GO", "DayZ", "HOS", "Isaac", "Shows", "Hearth", "WoT", "Agar.io"]
+
+viewers =  [1070, 472, 302, 239, 210, 171, 170, 90, 86, 71]
+
+#2.Now, use the plt.bar() to plot a bar graph using range(len(games)) and viewers as arguments.Feel free to pick a color, too (using color='____'). Then, use plt.show() to visualize it.
+plt.bar(range(len(games)),viewers, color ="slateblue")
+
+#3.Awesome, let's make the graph more informative by doing the following: Add a title, Add a legend, Add axis labels, Add ticks, Add tick labels (rotate if necessary)
+plt.legend(["Twitch"])
+plt.title("Featured Games Viewers")
+plt.xlabel("Games")
+plt.ylabel("Viewers")
+plt.subplot().set_xticks(range(len(games)))
+plt.subplot().set_xticklabels(games, rotation =30)
+plt.show()
+plt.clf()
+
+# Pie Chart: League of Legends Viewers' Whereabouts
+
+labels = ["US", "DE", "CA", "N/A", "GB", "TR", "BR", "DK", "PL", "BE", "NL", "Others"]
+
+countries = [447, 66, 64, 49, 45, 28, 25, 20, 19, 17, 17, 279]
+
+colors = ['lightskyblue', 'gold', 'lightcoral', 'gainsboro', 'royalblue', 'lightpink', 'darkseagreen', 'sienna', 'khaki', 'gold', 'violet', 'yellowgreen']
+
+#7.Let's make it more visually appealing and more informative.
+explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+#6.Then, use plt.pie() to plot a pie chart. Don't forget to throw in the countries variable and the colors = colors. Then, inside plt.pie(), we are going to: Add the explode, Add shadows, Turn the pie 345 degrees, Add percentages, Configure the percentages' placement. Also, we can add a title, And legends. Lastly, use plt.show() to visualize it.
+plt.pie(countries, explode=explode, colors=colors, shadow=True, startangle=345, autopct='%1.0f%%', pctdistance=1.15)
+plt.title("League of Legends Viewers' Whereabouts")
+plt.legend(labels, loc="right")
+plt.show()
+plt.clf()
+#8.The visualization should look something like. Open the hint for the full code.
+
+# Line Graph: Time Series Analysis
+
+hour = range(24)
+
+viewers_hour = [30, 17, 34, 29, 19, 14, 3, 2, 4, 9, 5, 48, 62, 58, 40, 51, 69, 55, 76, 81, 102, 120, 71, 63]
+
+#11.There is some uncertainty in these numbers because some people leave their browsers open. Let's account for a 15% error in the viewers_hour data. First, create a list containing the upper bound of the viewers_hour and call it y_upper. Then, create a list containing the lower bound of the viewers_hour and call it y_lower. Lastly, use plt.fill_between() to shade the error, with an alpha of 0.2.
+y_upper = [ i + (i*0.15) for i in viewers_hour]
+y_lower = [ i - (i*0.15) for i in viewers_hour]
+
+#10.Use plt.plot() to plot a line graph. Don't forget to throw in hour and viewers_hour.Then, add the title, the axis labels, legend, and ticks.
+plt.plot(hour, viewers_hour)
+plt.fill_between(hour, y_lower, y_upper, alpha =0.2)
+plt.title("Time Series")
+plt.legend(["2015-01-01"])
+plt.xlabel("Hour")
+plt.ylabel("Viewers")
+plt.subplot().set_xticks(hour)
+plt.show()
